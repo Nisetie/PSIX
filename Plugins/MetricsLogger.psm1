c3152c5a11d8ed4af7cb5ba226d38dba6fb1839e
Nisetie <gorelnikita@yandex.ru>
2022-07-20 00:46:33 +0700

    Update MetricsLogger.psm1

--- Plugins/MetricsLogger.psm1
+++ Plugins/MetricsLogger.psm1
@@ -1,9 +1,16 @@
+$currentHost = $instance;
+
 if (test-path ($rootPath + "\Live\MetricsLogger\")) {} 
 else {
 	New-Item -Path ($rootPath + "\Live\MetricsLogger\") -ItemType Directory | out-null
 }
 
-$currentHost = $instance;
+$t2 = $null;
+
+if (test-path ("$rootPath" + "\Live\MetricsLogger\" + "\$($currentHost.HostName).txt")) {
+    $t2 = Get-Content ("$rootPath" + "\Live\MetricsLogger\" + "\$($currentHost.HostName).txt") -Raw;
+    $t2 = ConvertFrom-Json -InputObject $t2;
+} 
 
 $h =  @{
     HostName = $currentHost.hostName
@@ -16,16 +23,41 @@ $h =  @{
 };
         
 $currentHost.updateScripts | %{ $h.($_.ElementName) = "$($_.CurrentValue)";  }
-        
-$data= @{
-    Host = $h
-    Templates = ($currentHost.templates | %{
+
+$templates = @();
+
+$templates += $currentHost.templates | %{
         $r = @{};
         $r.TemplateName = $_.updateScripts[0].templateName
         $r.UpdateDelta = $_.UpdateDelta.ToString()
         $_.updateScripts | %{ $r.($_.ElementName) = "$($_.CurrentValue)";  }
         return $r;
-    } ) 
-};           
+    }
+        
+$data= @{
+    Host = $h
+    Templates = $templates 
+};     
+
+
+if ($t2 -ne $null) {
+    foreach ($property in $t2.Host.psobject.Properties) {
+        if ($data.Host.Keys -notcontains $property.Name) {
+            $data.Host.$($property.Name) = $t2.Host.$($property.Name)
+        }    
+    }
+    $templates1 = $data.Templates.TemplateName;
+    foreach ($template in $t2.Templates) {
+        if ($templates1 -notcontains $template.TemplateName) { 
+            $t = @{};
+            foreach ($property in $template.psobject.Properties) {
+                $t.$($property.Name) = $template.$($property.Name);
+            }
+    
+            $data.Templates += $t;
+        }     
+    }
+}
+      
 
 $data | ConvertTo-JSON | Out-File -FilePath ( "$rootPath" + "\Live\MetricsLogger\" + "\$($currentHost.HostName).txt" ) -force 
